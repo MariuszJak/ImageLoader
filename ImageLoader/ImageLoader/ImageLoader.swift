30d7930f71d7555d949df6d201ed00363b691b67
MariuszJakowienko <mariusz.jakowienko@miquido.com>
2020-09-13 21:56:34 +0200

    Added basic load and caching mechanism.

--- ImageLoader/ImageLoader/ImageLoader.swift
+++ ImageLoader/ImageLoader/ImageLoader.swift
@@ -6,6 +6,28 @@
 //  Copyright © 2020 Mariusz Jakowienko. All rights reserved.
 //
 
-class ImageLoader {
+import Combine
+import UIKit
+import Foundation
 
+public class ImageLoader {
+    private static let cache = ImageCache()
+    
+    public static func loadImage(from url: URL) -> AnyPublisher<UIImage?, Never> {
+        let backgroundQueue = DispatchQueue(label: UUID().uuidString,
+                                            qos: .background)
+        if let image = cache[url] {
+            return Just(image).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { (data, response) -> UIImage? in return UIImage(data: data) }
+            .catch { error in return Just(nil) }
+            .handleEvents(receiveOutput: { image in
+                guard let image = image else { return }
+                self.cache[url] = image
+            })
+            .subscribe(on: backgroundQueue)
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
 }
