4f9bfe32f4caa69264164ca4ac4265fe57f7db6a
MariuszJakowienko <mariusz.jakowienko@miquido.com>
2020-09-13 20:52:21 +0200

    Added starting files.

---
+++ ImageLoader/ImageLoader/ImageLoader.swift
@@ -0,0 +1,11 @@
+//
+//  ImageLoader.swift
+//  ImageLoader
+//
+//  Created by Mariusz Jakowienko on 13/09/2020.
+//  Copyright © 2020 Mariusz Jakowienko. All rights reserved.
+//
+
+class ImageLoader {
+
+}
